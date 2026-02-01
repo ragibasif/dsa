4ace1b297bf3cdd85861692919c047bc4a7c643f
Ragib Asif <ragibasif@tuta.io>
2026-02-01 14:27:11 -0500

    chore: synchronize data and content
    Refreshed project data, content files, and related assets with the latest information. Ensured all synchronized resources are current and reflect recent changes from upstream sources.

--- bctci/strings.py
+++ bctci/strings.py
@@ -23,3 +23,42 @@ def to_lowercase(c):
         return chr(ord('Z') - ord(c) + ord('a'))
     return c
 
+# time: O(len(s))
+def split(s,d):
+    if not s:
+        return []
+    res = []
+    buf = []
+    for c in s:
+        if c == d:
+            res.append("".join(buf))
+            buf = []
+        else:
+            buf.append(c)
+    res.append("".join(buf))
+    buf = []
+    return res
+
+def join(arr,s):
+    buf = []
+    for i in range(len(arr)):
+        if i != 0:
+            buf.append(s)
+        for c in arr[i]:
+            buf.append(c)
+    res = "".join(buf)
+    return res
+
+
+def index_of(s,t):
+    res = -1
+    s_len = len(s)
+    t_len = len(t)
+    for i in s_len-t_len:
+        flag = True
+        for j in t_len:
+            if not s[i+j] == t[j]:
+                flag = False
+        if flag:
+            return i
+    return res
