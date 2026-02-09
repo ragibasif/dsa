4bbca6ece5e9885b6c520b6105388c29d69dd028
Ragib Asif <ragibasif@tuta.io>
2026-02-08 20:45:54 -0500

    chore: synchronize data and content
    Refreshed project data, content files, and related assets with the latest information. Ensured all synchronized resources are current and reflect recent changes from upstream sources.

--- cses/1071.cpp
+++ cses/1071.cpp
@@ -30,6 +30,35 @@ bool is_even(ll n){ return (n&1) == 0;}
 bool is_odd(ll n){ return (n&1) == 1;}
 
 void solve() {
+    ll y,x;
+    cin >> y >> x;
+
+    ll res = 1;
+
+    if (y > x) {
+	if (is_even(y)) {
+	    res = (y*y) - x + 1;
+	} else {
+	    res = ((y-1) * (y-1)) + 1 + x - 1;
+	}
+    }
+    else if (y < x) {
+	if (is_odd(x)) {
+	    res = (x*x) - y + 1;
+	}else {
+	    res = ((x-1) * (x-1)) + 1 + y - 1;
+	}
+    }
+    else {
+	if (is_even(y)) {
+	    res = (y*y) - x + 1;
+	} else {
+	    res = (x*x) - y + 1;
+	}
+    }
+
+    cout << res << '\n';
+
 }
 
 int main() {
@@ -38,7 +67,7 @@ int main() {
     cout.tie(nullptr);
 
     int t = 1;
-    //cin >> t; // multiple test cases
+    cin >> t; // multiple test cases
     while (t--) {
 	auto start = chrono::steady_clock::now();
 	solve();
